3d8dea7178e5d7ee7ce1ecc34ba3f4ca5999466e
surajkarki66 <suraj.karki500@protonmail.com>
2022-02-06 14:32:59 +0545

    Created development server

---
+++ todo-api/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = ['https://localhost:3000']
+
+app.add_middleware(CORSMiddleware, allow_origins=origins,
+                   allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
+
+@app.get("/")
+def read_root():
+    return {"ping": "pong"}
